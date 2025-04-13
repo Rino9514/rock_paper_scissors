985eaadba2a3c33f18399e636957973ba21d64c1
Rino <82651074+Rino9514@users.noreply.github.com>
2025-04-13 11:20:05 +0200

    Adding Human choice function, tested and approuved

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,11 +1,17 @@
 function getComputerChoice() {
     // 0 = rock, 1 = paper, 2 or others = scissors
-    let choices = (Math.floor(Math.random() * 3));
-    return choices === 0 ? "rock"
-         : choices === 1 ? "paper"
+    let choice = (Math.floor(Math.random() * 3));
+    return choice === 0 ? "rock"
+         : choice === 1 ? "paper"
          : "scissors";    
 }
 
-
+function getHumanChoice() {
+    let choice = prompt("rock, paper or scissors ?\n(if wrong default is scissors) : ");
+    return choice === "rock"  ? "rock"
+         : choice === "paper" ? "paper"
+         : "scissors";    
+}
 
 console.log(getComputerChoice());
+console.log(getHumanChoice());
