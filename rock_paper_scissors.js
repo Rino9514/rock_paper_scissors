2803c37287ed6c2a6be084ec8b53d988d5ebf0e5
Rino <82651074+Rino9514@users.noreply.github.com>
2025-04-13 12:06:35 +0200

    Adding round function, verified

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     // 0 = rock, 1 = paper, 2 or others = scissors
     let choice = (Math.floor(Math.random() * 3));
@@ -8,10 +11,35 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let choice = prompt("rock, paper or scissors ?\n(if wrong default is scissors) : ");
-    return choice === "rock"  ? "rock"
-         : choice === "paper" ? "paper"
+    return choice.toLowerCase() === "rock"  ? "rock"
+         : choice.toLowerCase() === "paper" ? "paper"
          : "scissors";    
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound(humanChoice,computerChoice) {
+    if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You win ! Your choice : " + humanChoice + " beat the computer choice : " + computerChoice );
+        ++humanScore;
+    }
+    else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("You win ! Your choice : " + humanChoice + " beat the computer choice : " + computerChoice );
+        ++humanScore;
+    }
+    else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("You win ! Your choice : " + humanChoice + " beat the computer choice : " + computerChoice );
+        ++humanScore;
+    }
+    else if (humanChoice === computerChoice) {
+        console.log("It's a draw ! both get : "+ computerChoice );
+    }
+    else {
+        console.log("You lose ! Your choice : " + humanChoice + " get beaten by computer choice : " + computerChoice );
+        ++computerScore;
+    }
+    console.log("Human score : " + humanScore + " Computer score : " + computerScore);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
